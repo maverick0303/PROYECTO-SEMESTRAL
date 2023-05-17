9d8d50062387af5691522e65a58a6ce9d4cc3b51
Nopesito <er.pino@duocuc.cl>
2023-05-17 18:54:25 -0400

    arreglo css/js/img de admin y tienda

--- tiendita/urls.py
+++ tiendita/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import tienda,bombo
+from .views import (tienda,bombo,a_bombo,a_prod_cuerda,a_prod_idiofono,a_prod_percusion,
+                    a_prod_viento,a_prod_nuevo,a_tienda,prod_cuerda,prod_idiofono,prod_percusion,prod_viento,feriados)
 urlpatterns = [
     path('',tienda,name="tienda"),
     path('bombo/',bombo,name="bombo"),
+    path('a_bombo/',a_bombo,name="a_bombo"),
+    path('a_prod_cuerda/',a_prod_cuerda,name="a_prod_cuerda"),
+    path('a_prod_idiofono/',a_prod_idiofono,name="a_prod_idiofono"),
+    path('a_prod_percusion/',a_prod_percusion,name="a_prod_percusion"),
+    path('a_prod_viento/',a_prod_viento,name="a_prod_viento"),
+    path('a_prod_nuevo/',a_prod_nuevo,name="a_prod_nuevo"),
+    path('a_tienda/',a_tienda,name="a_tienda"),
+    path('prod_cuerda/',prod_cuerda,name="prod_cuerda"),
+    path('prod_idiofono/',prod_idiofono,name="prod_idiofono"),
+    path('prod_percusion/',prod_percusion,name="prod_percusion"),
+    path('prod_viento/',prod_viento,name="prod_viento"),
+    path('feriados/',feriados,name="feriados"),
 ]
--- tiendita/views.py
+++ tiendita/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
 
 # Create your views here.
-def bombo(request):
-    return render(request,'tiendita/articulos/bombo.html')
+
+#ADMIN
+def a_bombo(request):
+    return render(request,'tiendita/admin/bombo.html')
+
+def a_prod_cuerda(request):
+    return render(request,'tiendita/admin/prod_cuerda.html')
+
+def a_prod_idiofono(request):
+    return render(request,'tiendita/admin/prod_idiofono.html')
+
+def a_prod_percusion(request):
+    return render(request,'tiendita/admin/prod_percusion.html')
+
+def a_prod_viento(request):
+    return render(request,'tiendita/admin/prod_viento.html')
+
+def a_prod_nuevo(request):
+    return render(request,'tiendita/admin/prod_nuevo.html')
+
+def a_tienda(request):
+    return render(request,'tiendita/admin/tienda.html')
+
+
+#TIENDA
+def prod_cuerda(request):
+    return render(request,'tiendita/tienda/prod_cuerda.html')
+
+def prod_idiofono(request):
+    return render(request,'tiendita/tienda/prod_idiofono.html')
+
+def prod_percusion(request):
+    return render(request,'tiendita/tienda/prod_percusion.html')
+
+def prod_viento(request):
+    return render(request,'tiendita/tienda/prod_viento.html')
+
+def feriados(request):
+    return render(request,'tiendita/tienda/feriados.html')
 
 def tienda(request):
     nombre="Leo"
@@ -10,3 +47,11 @@ def tienda(request):
         "nomb":nombre
     }
     return render(request,'tiendita/tienda/tienda.html',contexto)
+
+
+#PRODUCTO
+def bombo(request):
+    return render(request,'tiendita/articulos/bombo.html')
+
+
+
