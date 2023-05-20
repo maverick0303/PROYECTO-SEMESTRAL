64bd3b2ed33ad3ba8147bf5ce59ce7b2fa78322d
Nopesito <er.pino@duocuc.cl>
2023-05-19 23:39:27 -0400

    plantilla

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -76,15 +76,8 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/orcl',
-        'USER': 'maxwell',
-        'PASSWORD': 'maria1234',
-        'TEST':{
-            'USER': 'default_test',
-            'TBLSPACE': 'default_test_tbls',
-            'TBLSPACE_TMP': 'default_test_tbls_tmp',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
