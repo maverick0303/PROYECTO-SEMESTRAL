6bedb9d56dc5d9ceba11e727e3b3658b49a00aae
Nopesito <er.pino@duocuc.cl>
2023-06-09 12:21:15 -0400

    api le falta

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api_rest.views import lista_comuna, lista_region
+from api_rest.views import lista_comuna, lista_region,lista_region_id
 
 urlpatterns = [
     path('lista_comuna/',lista_comuna,name="lista_comuna"),
     path('lista_region/',lista_region,name="lista_region"),
+    path('lista_region_id/<id>',lista_region_id,name="lista_region_id"),
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -37,3 +37,25 @@ def lista_region(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def lista_region_id(request, id):
+    try:
+        region = Region.objects.get(id_region = id)
+    except Region.DoesNotExist:
+        return Response(serializer.errors, status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = regionSerializers(region)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = regionSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        region.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
