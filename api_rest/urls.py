665455bcbf4f281a5ed00ab832ddc06d84489b92
Nopesito <er.pino@duocuc.cl>
2023-06-09 19:51:24 -0400

    api lista

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from api_rest.views import lista_comuna, lista_region,lista_region_id
+from api_rest.views import lista_comuna, lista_region,lista_region_id,lista_comuna_id
 
 urlpatterns = [
     path('lista_comuna/',lista_comuna,name="lista_comuna"),
     path('lista_region/',lista_region,name="lista_region"),
     path('lista_region_id/<id>',lista_region_id,name="lista_region_id"),
+    path('lista_comuna_id/<id>',lista_comuna_id,name="lista_comuna_id"),
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -24,6 +24,27 @@ def lista_comuna(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def lista_comuna_id(request, id):
+    try:
+        comuna = Comuna.objects.get(id_comuna = id)
+    except Region.DoesNotExist:
+        return Response(serializer.errors, status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = comunaSerializers(comuna)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = comunaSerializers(comuna, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        comuna.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+@csrf_exempt
 @api_view(['GET', 'POST'])
 def lista_region(request):
     if request.method == 'GET':
@@ -49,7 +70,7 @@ def lista_region_id(request, id):
         serializer = regionSerializers(region)
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = regionSerializers(data=request.data)
+        serializer = regionSerializers(region, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
