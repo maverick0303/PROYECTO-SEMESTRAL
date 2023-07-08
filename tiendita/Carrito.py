b5cfd33dc704993dcd7d7cc6fe93810f669ccb3e
maria <ma.yeguez@duocuc.cl>
2023-07-08 12:03:02 -0400

    "NO DEJA COMPRAR SI YA AGOTO EL STOCK Y NO COMPRA MAS DEL STOCK"

--- tiendita/Carrito.py
+++ tiendita/Carrito.py
@@ -10,7 +10,7 @@ class Carrito:
 
         if not carrito:
             # Si no existe un carrito en la sesión, se crea uno vacío.
-            self.session["carrito"] = {} 
+            self.session["carrito"] = {}
             self.carrito = self.session["carrito"]
         else:
             # Si ya existe un carrito en la sesión, se asigna a la variable de instancia.
@@ -20,24 +20,26 @@ class Carrito:
         """
         Agrega un producto al carrito de compras.
         Si el producto ya está en el carrito, se incrementa la cantidad y el acumulado de precio.
-        Si el producto tiene stock disponible, se disminuye el stock en 1.
+        Si el producto tiene stock disponible, se agrega la cantidad disponible al carrito.
         """
         id = str(producto.cod_producto)
         if id not in self.carrito.keys():
             # Si el producto no está en el carrito, se agrega con cantidad 1 y se actualizan los datos.
             self.carrito[id] = {
-                "product_id" : producto.cod_producto,
-                "nombre" : producto.nombre,
+                "product_id": producto.cod_producto,
+                "nombre": producto.nombre,
                 "acumulado": producto.precio,
-                "cantidad" : 1,
-                "stock": producto.stock 
+                "cantidad": 1,
+                "stock": producto.stock
             }
         else:
-            if self.carrito[id]["stock"] > 0:
-                # Si el producto está en el carrito y tiene stock disponible, se incrementa la cantidad y el acumulado.
-                self.carrito[id]["cantidad"] += 1
-                self.carrito[id]["acumulado"] += producto.precio
-                self.carrito[id]["stock"] -= 1
+            stock_disponible = self.carrito[id]["stock"]
+            if stock_disponible > 0:
+                cantidad_a_agregar = min(stock_disponible, producto.stock)
+                # Si el producto está en el carrito y tiene stock disponible, se agrega la cantidad disponible al carrito.
+                self.carrito[id]["cantidad"] += cantidad_a_agregar
+                self.carrito[id]["acumulado"] += producto.precio * cantidad_a_agregar
+                self.carrito[id]["stock"] -= cantidad_a_agregar
 
         # Actualizar el stock en el objeto producto original
         producto.stock = self.carrito[id]["stock"]
@@ -52,18 +54,24 @@ class Carrito:
         """
         Resta un producto del carrito de compras.
         Si la cantidad del producto en el carrito es mayor a 1, se disminuye la cantidad y el acumulado de precio.
-        Si la cantidad del producto en el carrito es 1, se elimina el producto del carrito.
+        Si la cantidad del producto en el carrito es 1, no se elimina el producto del carrito.
         """
         id = str(producto.cod_producto)
-        self.carrito[id]["cantidad"] -= 1
-        self.carrito[id]["acumulado"] -= producto.precio
-        if self.carrito[id]["cantidad"] <= 0:
-            self.delete(producto)
+        if self.carrito[id]["cantidad"] > 1:
+            self.carrito[id]["cantidad"] -= 1
+            self.carrito[id]["acumulado"] -= producto.precio
+            self.carrito[id]["stock"] += 1
         else:
-            self.session["carrito"] = {}
+            self.carrito[id]["cantidad"] = 1
+            self.carrito[id]["acumulado"] = producto.precio
+            self.carrito[id]["stock"] += 1
+
+        # Actualizar el stock en el objeto producto original
+        producto.stock = self.carrito[id]["stock"]
+        producto.save()
         self.save()
 
-    def save(self): 
+    def save(self):
         """
         Guarda los cambios realizados en el carrito en la sesión del usuario.
         """
@@ -77,7 +85,7 @@ class Carrito:
         id = str(producto.cod_producto)
         if id in self.carrito:
             del self.carrito[id]
-            self.save() 
+            self.save()
 
     def clean(self):
         """
